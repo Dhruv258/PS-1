cfc241ec7466268800e05a8d053870b3a4545ac6
Dhruv258 <107675388+Dhruv258@users.noreply.github.com>
2022-07-25 17:06:22 +0530

    Update mongopass.py

--- mongopass.py
+++ mongopass.py
@@ -1,6 +1,7 @@
-mongopass = "mongodb+srv://admin:JsQ1oVUM28BCPT96@cluster0.x553t.mongodb.net/?retryWrites=true&w=majority"
-from flask_pymongo import PyMongo
+# Assign your MongoDB URI to the mongopass variable
+from pymongo import MongoClient
+mongopass = "mongodb+srv://admin:1password2@cluster0.ppse8zf.mongodb.net/?retryWrites=true&w=majority"
 
-
-
-
+client = MongoClient(mongopass)
+db = client["shipment_details"]
+collection = db["test"]
